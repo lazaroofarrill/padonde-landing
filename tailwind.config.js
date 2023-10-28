5240aa2f23e12cab3f0734c96e27acd6f2d72faf
lazaroofarrill <lazaroofarrill@gmail.com>
2023-10-27 23:00:32 -0400

    added husky and pre-commit hooks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-    theme: {
-        extend: {
-            colors: {
-                primary: '#09072B',
-                accent: '#C91480',
-                surface: '#F6F6FF',
-                secondary: '#87D9C8'
-            },
-            boxShadow: {
-                '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.8)',
-            }
-        }
-    },
-    plugins: []
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#09072B',
+				accent: '#C91480',
+				surface: '#F6F6FF',
+				secondary: '#87D9C8'
+			},
+			boxShadow: {
+				'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.8)'
+			}
+		}
+	},
+	plugins: []
 };
